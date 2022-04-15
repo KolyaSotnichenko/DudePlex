6da294db459be1c4b7478202f77eafa25f946411
Kolya Sotnichenko <user.email>
2022-04-15 13:44:19 +0300

    fixed translate

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -14,7 +14,7 @@ const Catalog = () => {
     return (
         <>
             <PageHeader>
-                {category === cate.movie ? 'Movies' : 'TV Series'}
+                {category === cate.movie ? 'Фільми' : 'Серіали'}
             </PageHeader>
             <div className="container">
                 <div className="section mb-3">
