c8e1fcdb81347f4b10ac30d91e2a01dca2985971
Kolya Sotnichenko <user.email>
2022-04-18 12:20:17 +0300

    added fetching movies ids from firebase

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PageHeader from '../../components/page-header/PageHeader';
 import MovieList from '../../components/movie-list/MovieList';
 
@@ -6,9 +6,13 @@ import { category, movieType} from '../../api/tmdbApi';
 import ProfileBlock from './ProfileBlock';
 import { useHistory } from 'react-router-dom';
 
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from '../../firebase';
+
 const Profile = () => {
 
     let history = useHistory()
+    let tmdbids
 
     useEffect(() => {
         let authToken = sessionStorage.getItem("Auth Token")
@@ -17,6 +21,23 @@ const Profile = () => {
             history.push('/signin')
         }
     }, [])
+
+    useEffect(() => {
+        auth.onAuthStateChanged(user => {
+            console.log(user.uid)
+            const docRef = doc(db, "view-later-list", user.uid)
+
+            getDoc(docRef).then(snapshot => {
+                if(snapshot.exists()){
+                    console.log("Document data:", snapshot.data());
+                    tmdbids = Object.values(snapshot.data())
+                }else{
+                    console.log("No such document!");
+                }
+            })
+        })
+
+    }, [tmdbids])
 
     return(
         <>
